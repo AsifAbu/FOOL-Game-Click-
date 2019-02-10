4bd38b73e7685bef0441e3d41644a8c66bb52d1c
Asif <40964477+AsifAbu@users.noreply.github.com>
2019-02-11 00:32:37 +0600

    Add files via upload

---
+++ Click/Click/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Click
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(75, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(75, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "3";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(75, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Tag = "5";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(75, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 50);
+            this.button4.TabIndex = 7;
+            this.button4.Tag = "7";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(75, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 50);
+            this.button5.TabIndex = 9;
+            this.button5.Tag = "9";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button_click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(197, 32);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 50);
+            this.button6.TabIndex = 2;
+            this.button6.Tag = "2";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button_click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Red;
+            this.button7.Location = new System.Drawing.Point(197, 88);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(116, 50);
+            this.button7.TabIndex = 4;
+            this.button7.Tag = "4";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button_click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Red;
+            this.button8.Location = new System.Drawing.Point(197, 144);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(116, 50);
+            this.button8.TabIndex = 6;
+            this.button8.Tag = "6";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button_click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Red;
+            this.button9.Location = new System.Drawing.Point(197, 200);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(116, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "8";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button_click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Red;
+            this.button10.Location = new System.Drawing.Point(197, 256);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(116, 50);
+            this.button10.TabIndex = 10;
+            this.button10.Tag = "10";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button_click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Score :";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(173, 322);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(18, 18);
+            this.score.TabIndex = 12;
+            this.score.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Instructions : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 14);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "1. Clicking when Blue adds 10 points.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(72, 420);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 14);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "2. Clicking whwn Red deducts 10 points.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(143, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Click Counter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(390, 483);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ClickCounter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Click/Click/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Click
+{
+    public partial class Form1 : Form
+    {
+        List<Button> button = new List<Button>();
+        int sc = 0;
+        int check;
+        int t = 1300;
+        Random ran = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.BackColor == Color.Blue)
+            {
+                sc = sc + 10;
+            }
+            else
+            {
+                sc = sc - 10;
+            }
+
+            score.Text = sc.ToString();
+
+        }
+
+        public void makeBlue()
+        {
+
+            button1.BackColor = Color.Red;
+            button2.BackColor = Color.Red;
+            button3.BackColor = Color.Red;
+            button4.BackColor = Color.Red;
+            button5.BackColor = Color.Red;
+            button6.BackColor = Color.Red;
+            button7.BackColor = Color.Red;
+            button8.BackColor = Color.Red;
+            button9.BackColor = Color.Red;
+            button10.BackColor = Color.Red;
+
+            check = ran.Next(1, 10);
+            if (check == Convert.ToInt32(button1.Tag))
+            {
+                button1.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button2.Tag))
+            {
+
+                button2.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button3.Tag))
+            {
+                button3.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button4.Tag))
+            {
+                button4.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button5.Tag))
+            {
+                button5.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+
+            if (check == Convert.ToInt32(button6.Tag))
+            {
+                button6.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button7.Tag))
+            {
+                button7.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button8.Tag))
+            {
+                button8.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button9.Tag))
+            {
+                button9.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+
+            if (check == Convert.ToInt32(button10.Tag))
+            {
+                button10.BackColor = Color.Blue;
+                timer1.Interval = t;
+
+            }
+}
+       private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            makeBlue();
+
+        }
+    }
+}
